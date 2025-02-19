b8b23e1d80b2d9ea36d4df882f8e6f69b3b11007
“Matt <mschauer@leftfield-solutions.com>
2025-02-19 10:46:32 -0600

    removed devtools on production build

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,6 +6,8 @@ import { theme } from '../theme';
 import { ThemeProvider } from '@mui/material';
 import Footer from '@/components/layout/footer';
 
+const showRouterIcon = process.env.NODE_ENV === 'development' ? true : false;
+
 export const Route = createRootRoute({
   component: () => (
     <>
@@ -18,7 +20,7 @@ export const Route = createRootRoute({
           </div>
         </ThemeProvider>
       </StyledEngineProvider>
-      <TanStackRouterDevtools />
+      {showRouterIcon && <TanStackRouterDevtools />}
     </>
   )
 });
