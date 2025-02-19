99a5292e729bbafbecb036bc768514468d0b6f09
“Matt <mschauer@leftfield-solutions.com>
2025-02-19 09:03:57 -0600

    updated to hashrouting

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { RouterProvider, createRouter, createHashHistory } from '@tanstack/react-router';
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
@@ -8,7 +8,12 @@ import { routeTree } from './routeTree.gen';
 import './styles.css';
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+
+const hashHistory = createHashHistory();
+const router = createRouter({
+  routeTree,
+  history: hashHistory
+});
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
